2a862fae4621b7f3d7754d93c3f92aed09f547c5
En.Darren <145190588+Ngabe93@users.noreply.github.com>
2024-08-28 21:01:14 +0100

    Update OUTPUTS.tf

--- Project-2-Networking/VPC_PEERING/OUTPUTS.tf
+++ Project-2-Networking/VPC_PEERING/OUTPUTS.tf
@@ -2,5 +2,5 @@ output"ip_instance_1"{
     value=aws_instance.INSTANCE_1.public_ip
 }
 output"ip_instance_2"{
-    value=aws_instance.INSTANCE_2.public_ip
+    value=aws_instance.INSTANCE_2.private_ip
 }
